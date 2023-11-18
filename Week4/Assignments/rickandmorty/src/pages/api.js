79356b7754fae7feee0e1e94c79811c3c9d4d477
betulyurtman <betullyurtman@gmail.com>
2023-11-18 13:47:52 +0300

    Start: Rick and Morty API Project

---
+++ Week4/Assignments/rickandmorty/src/pages/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseURL = 'https://rickandmortyapi.com/api';
+
+const axiosInstance = axios.create({
+    baseURL,
+});
+
+export const fetchCharacters = async () => {
+    const response = await axiosInstance.get('/character');
+    return response.data.results; 
+};
